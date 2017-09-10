4e3673885c8b193ab44420b73de7da889c2d847b
adnanSharif <adnan.sharif.mail@gmail.com>
2017-09-11 02:56:09 +0600

    primary UI added

---
+++ app/src/main/java/mist/ac/bd/diabatesepredicotor/PredictionResult.java
@@ -0,0 +1,13 @@
+package mist.ac.bd.diabatesepredicotor;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class PredictionResult extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_prediction_result);
+    }
+}
